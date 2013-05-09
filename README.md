a847defad8b77f1919ebf0cd201e3958b56fc4f4
Jurre Stender <jurrestender@hotmail.com>
2013-05-09 16:16:43 +0200

    initial public commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Gitlab Hooks, yo!
+=================
+
+These are the hooks that we use to post to Asana and send a message to
+HipChat whenever we push to a GitLab repository.
+
+To use this simply fill out the env.rb file with the required
+credentials.
+
---
+++ env.rb
@@ -0,0 +1,4 @@
+ENV['auth_token'] = 'your Asana authentication token'
+ENV['username'] = 'username for sinatra app http basic auth'
+ENV['password'] = 'password for sinatra app http basic auth'
+ENV['room_name'] = 'the HipChat roomname that you want to post to'
