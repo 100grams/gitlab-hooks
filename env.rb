dfefe077153752b894b3932c91fff390535da39e
Jurre Stender <jurrestender@hotmail.com>
2013-07-22 12:56:55 +0200

    Add example hipchat token to env file

--- env.rb
+++ env.rb
@@ -1,3 +1,4 @@
 ENV['auth_token'] = 'your Asana authentication token'
+ENV['hipchat_token'] = 'your hipchat authentication token'
 ENV['username'] = 'username for sinatra app http basic auth'
 ENV['password'] = 'password for sinatra app http basic auth'
