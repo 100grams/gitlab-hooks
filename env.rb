5eeea49ee047a3aa03d88545266c8498a1acebd7
Rotem Rubnov <rotem.rubnov@100grams.nl>
2018-01-23 09:16:40 +0100

    removed private tokens

--- env.rb
+++ env.rb
@@ -1,4 +1,4 @@
-ENV['asana_api_token'] = 'maXa3gd.WexHm0SjxPP0b6Zfqp4RPkmM' 
-ENV['hipchat_token'] = 'b18ff1b9fe8b6bca92748483fa0e64'
+ENV['asana_api_token'] = <your_asana_token> 
+ENV['hipchat_token'] = <your_token>
 ENV['username'] = 'username for sinatra app http basic auth'
 ENV['password'] = 'password for sinatra app http basic auth'
